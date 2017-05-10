c6e48e66e52aefabb55ea112563afe04ef947c4d
lanseria <zhangchao564265135@hotmail.com>
2017-05-11 00:47:29 +0800

    upload

---
+++ app/controllers/index.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var path = require('path');
+var Member = require('../models/member');
+var ArrageClass = require('../../lpsdk/arrageClass').ArrageClass;
+
+exports.index = function(req, res){
+  res.render('index', {
+    title: '排值班系统',
+  });
+}
+
+exports.details = function(req, res){
+  res.render('details', {
+    title: '排值班系统',
+  });
+}
+
+exports.demofiles = function(req, res){
+  var fileName = req.params.fileName;
+  var filePath = path.join(__dirname, '../../public/demo_xls/', fileName);
+  console.log(filePath);
+  var stats = fs.statSync(filePath);
+  if (stats.isFile()) {
+    res.set({
+     'Content-Type': 'application/octet-stream',
+     'Content-Disposition': 'attachment; filename='+fileName,
+     'Content-Length': stats.size
+    });
+    fs.createReadStream(filePath).pipe(res);
+  }
+  else{
+    res.end(404);
+  }
+}
+
+exports.download = function(req, res){
+  var client = new ArrageClass({
+    'row': 6,
+    'col': 5,
+    'many': 100,
+  })
+  var suc = client.execute(function(err, suc){
+    if (err) {console.log(err);}
+    else{
+      var fileName = 'output.xlsx';
+      var filePath = path.join(suc);
+      console.log(filePath);
+      var stats = fs.statSync(filePath);
+      if (stats.isFile()) {
+        res.set({
+         'Content-Type': 'application/octet-stream',
+         'Content-Disposition': 'attachment; filename='+fileName,
+         'Content-Length': stats.size
+        });
+        fs.createReadStream(filePath).pipe(res);
+      }
+      else{
+        res.end(404);
+      }
+    }
+  });
+}
+
+
+exports.save = function(req, res){
+  var xlsFileData = req.files.uploadXls;
+  var filePath = xlsFileData.path;
+  var name = req.body.uploadName;
+  var number = req.body.uploadNumber;
+  var originalFilename = xlsFileData.originalFilename;
+  if (originalFilename) {
+    fs.readFile(filePath, function(err, data){
+      console.log(xlsFileData.type);
+      // var type = xlsFileData.type.split('/')[1];
+      var type = 'xls'
+      var xlsFile = name + '.' + type;
+      var newPath = path.join(__dirname, '../../', '/public/upload/' + xlsFile);
+      fs.writeFile(newPath, data, function(err){
+        var member = new Member();
+        member.name = name;
+        member.number = number;
+        member.save(function(err, member){
+          return res.redirect('/index');
+        })
+      })
+    })
+  }
+  else{
+    return res.redirect('/index');
+  }
+}
---
+++ route.js
@@ -0,0 +1,34 @@
+var Index = require('./app/controllers/index');
+var multipart = require('connect-multiparty');
+var fs = require('fs');
+var trim = require('trim');
+var path = require('path');
+var _ = require('lodash');
+
+var multipartMiddleware = multipart();
+
+
+module.exports = function(app){
+  //pre handle user
+  app.use(function(req, res, next){
+    var deviceAgent = req.headers["user-agent"].toLowerCase();
+    var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
+    if(agentID){
+      return res.send('仅支持PC端');
+    }else{
+      next();
+    }
+  })
+
+  // index page
+  app.get('/', Index.index);
+  app.get('/index', Index.index);
+
+  app.get('/details', Index.details);
+
+  app.post('/uploadXls', multipartMiddleware, Index.save);
+
+  app.get('/demofiles/:fileName', Index.demofiles);
+
+  app.get('/download', Index.download);
+}
